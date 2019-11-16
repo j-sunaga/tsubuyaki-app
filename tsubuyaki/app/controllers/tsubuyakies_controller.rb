70d60f7bb99131213fb0fe8f7d54436c4c8646b7
sunaga <sunaga@gmail.com>
2019-11-16 16:52:14 +0700

    新規つぶやき登録機能の実装(new/createアクションとnewビューの作成)

--- tsubuyaki/app/controllers/tsubuyakies_controller.rb
+++ tsubuyaki/app/controllers/tsubuyakies_controller.rb
@@ -5,4 +5,23 @@ class TsubuyakiesController < ApplicationController
     @tsubuyakies = Toukou.all
   end
 
+  def new
+    @tsubuyaki = Toukou.new
+  end
+
+  def create
+    @tsubuyaki = Toukou.new(tsubuyaki_params)
+      if @tsubuyaki.save
+        redirect_to tsubuyakies_path, notice: "つぶやきました！"
+      else
+        render action: :new
+      end
+  end
+
+  private
+
+  def tsubuyaki_params
+    params.require(:toukou).permit(:content)
+  end
+
 end
