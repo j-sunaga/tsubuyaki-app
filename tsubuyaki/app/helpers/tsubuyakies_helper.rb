bf098e19f2cb9c3c310d9ca52f425057f5b434f2
sunaga <sunaga@gmail.com>
2019-11-17 11:00:29 +0700

    つぶやきの「新規登録or 編集」かによってルーティングを分けるhelperメソッドを実装

--- tsubuyaki/app/helpers/tsubuyakies_helper.rb
+++ tsubuyaki/app/helpers/tsubuyakies_helper.rb
@@ -1,2 +1,9 @@
 module TsubuyakiesHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm' || action_name == 'create'
+      confirm_tsubuyakies_path
+    elsif action_name == 'edit'
+      tsubuyaky_path
+    end
+  end
 end
